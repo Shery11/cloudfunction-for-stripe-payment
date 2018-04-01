cfbc909c021f66ee135732db8985ee47f9d61b6b
Shaharyar Kirmani <shery_kirmani@hotmail.com>
2018-04-01 18:44:05 +0500

    updated

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,27 @@ const express = require('express');
 const stripe = require('stripe')('sk_test_4ResYzIWW4eq8z5Ct5grgT1X');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+const admin = require('firebase-admin');
+admin.initializeApp({
+	credential: admin.credential.applicationDefault(),
+    databaseURL: "https://ionic-crud-b95b2.firebaseio.com"
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 const app = express();
+var router = express.Router();
 
-app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 
-app.post('/stripe',(request,response)=>{
+
+router.post('/stripe',(request,response)=>{
 
 	console.log(request.body);
+
+	var userId = request.body.userId;
+	var spins = request.body.spins;
 	
 	var stripetoken = request.body.stripetoken;
 	var amountPayable = request.body.amountPayable;
@@ -24,13 +34,24 @@ app.post('/stripe',(request,response)=>{
 		source : stripetoken
 	},function(err,charge){
 		if(err){
-
+			console.log(err);
 			response.json({success: false, err: err})
 
 		}else{
+			console.log("success");
+			var spins = spins + 5;
+            var usersRef = admin.database().ref(`users/${userId}`);
+			usersRef.update({
+			  "spins": spins
+			});
             response.json({success : true , message : charge});
 		}
 	})
 })
 
-exports.stripe = functions.https.onRequest(app);
+app.use(router);
+
+// exports.stripe = functions.https.onRequest(app);
+app.listen(8000, function(){
+	console.log("Server started on 8000")
+})
